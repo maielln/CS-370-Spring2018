0ce333a19b999f02451322fbe59a415e632f5e7d
OriginalUsername1 <35743045+OriginalUsername1@users.noreply.github.com>
2018-01-23 14:44:30 -0600

    Update ART2.c

--- ART2.c
+++ ART2.c
@@ -1,6 +1,171 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <stdbool.h>
 
-int main(){
-   printf("Hi");
-   return EXIT_SUCCESS;
+/*
+{$G+}{$N+}{$E+}{$X+}{$D-}
+{$M 16384,0,655360}
+{                                                                           }
+{  AT-Robots 2,  Copyright 1997,'99, Ed T. Toton III, All Rights Reserved.  }
+{                                                                           }
+{    This Source code was not originally intended for general distribution. }
+{ It is not optimized for anything in particular. It is not even intended   }
+{ to be very clear or intelligible to anyone but myself. Nor is it designed }
+{ to be portable. It is being distributed under a variant of the BSD        }
+{ license (see below)                                                       }
+{                                                                           }
+{ Designed for Turbo Pascal 6.0                                             }
+{                                                                           }
+
+   Code between {FIFI} and {/FIFI} tags were added by FiFi LaRoo. These
+   tags may eventually be removed (especially as they get modified in the
+   future), but are here for the time being to keep track of where the
+   changes were made.
+
+
+Copyright (c) 1999, Ed T. Toton III. All rights reserved.
+
+Bug fixes and additional additions Copyright (c) 2014, William "Amos" Confer
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+   Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+   Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+   All advertising materials mentioning features or use of this software
+   must display the following acknowledgement:
+
+        This product includes software developed by Ed T. Toton III &
+        NecroBones Enterprises.
+
+   No modified or derivative copies or software may be distributed in the
+   guise of official or original releases/versions of this software. Such
+   works must contain acknowledgement that it is modified from the original.
+
+   Neither the name of the author nor the name of the business or
+   contributers may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND ANY
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+//-------------------VARIABLE DECLARATION---------------------------------------------------------------
+
+char progname[10] = "AT-Robots", version[4] = "2.11",
+    cnotice1[35] = "Copyright 1997 '99, Ed T. Toton III", cnotice2[19] = "All Rights Reserved",
+    cnotice3[37] = "Copyright 2014, William 'Amos' Confer", main_filename[4] = "ATR2",
+    robot_ext[4] = ".AT2", locked_ext[4] = ".ATL", config_ext[4] = ".ATS",
+    compile_ext[4] = ".CMP", report_ext[4] = ".REP";
+int minint = -32768, maxint = 32787;
+bool T = true, F = false;
+
+//Robots
+int max_robots = 31, max_code = 1023, max_op = 3, stack_size = 256,
+    stack_base = 768, max_ram = 1023, max_vars = 256, max_labels = 256,
+    acceleration = 4, turn_rate = 8, max_vel = 4, max_missiles = 1023,
+    missile_spd = 32, hit_range = 14, blast_radius = 25, crash_range = 8,
+    max_sonar = 250, com_queue = 512, max_queue = 255, max_config_points = 12,
+    max_mines = 63, mine_blast = 35;
+
+//simulator & graphics
+double screen_scale = 0.46;
+int screen_x = 5, screen_y = 5, robot_scale = 6, default_delay = 20,
+    default_slice = 5, max_robot_lines = 8, mine_circle = trunc(35*0.46) + 1, //(mine_blast*screen_scale)
+    blast_circle = trunc(25*0.46) + 1, mis_radius = trunc(14/2) + 1; //(blast_radius*screen_scale), (hit_range/2)
+
+//Gray50 : FillPatternType = (0X AA, 0X 55, 0X AA, 0X 55, 0X AA, 0X 55, 0X AA, 0X 55);
+
+typedef struct operation_record {
+    int op[3]; //[max_op]
+} op_rec;
+
+typedef struct configuration_record {
+    int scanner, weapon, armor, engine, heatsinks, shield, mines;
+} config_rec;
+
+typedef struct mine_record {
+    unsigned x, y;
+    int detect, yield;
+    bool detonate;
+} mine_rec;
+
+typedef struct robot_record {
+    bool is_locked;
+    int mem_watch;
+    //OH GOD WHY
+    unsigned x, y, lx, ly, xv, yv, speed, shotstrength, damageadj, speedadj, meters;
+    int hd, thd, lhd, spd, tspd, armor, larmor, heat, lheat, ip, plen, scanarc, accuracy,
+        shift, err, delay_left, robot_time_limit, max_time, time_left, lshift, arc_count,
+        sonar_count, scanrange, last_damage, last_hit, transponder, shutdown, channel, lendarc,
+        endarc, lstartarc, startarc, mines;
+    int tx[8], ltx[8], ty[8], lty[8]; //[max_robot_lines]
+    unsigned wins, trials, kills, deaths, startkills, shots_fired, match_shots, hits, damage_total,
+        cycles_lived, error_count;
+    config_rec config;
+    char name[31], fn[255], errorlog[255];
+    bool shields_up, lshields, overburn, keepshift, cooling, won;
+    //prog_type code;
+    int ram[1023]; //[max_ram]
+    mine_rec mine[63]; //[max_mines]
+} robot_rec;
+
+typedef struct missile_record {
+    unsigned x, y, lx, ly, mult, mspd;
+    int source, a, hd, rad, lrad, max_rad;
+} missile_rec;
+
+op_rec prog_type[1023]; //max_code
+char parsetype[63][3][16]; //[][max_op][]
+
+//robot variables
+int num_robots;
+robot_rec *robot_ptr[31+2]; //[max_robots+2]
+missile_rec *missile_arr[1023]; //[max_missiles]
+
+//compiler variables
+char f;
+int numvars, numlabels, maxcode, lock_pos, lock_dat;
+char varname[63][256][31]; //[][max_vars][max_var_len]
+int varloc[256]; //[max_vars]
+char labelname[63][256][31]; //[][max_vars][max_var_len]
+int labelnum[63][256]; //[][256]
+bool show_source, compile_only;
+char lock_code[63];
+
+//simulator/graphics variables
+bool bout_over, step_loop;
+int step_mode, temp_mode, step_count;
+
+bool old_shields, insane_missiles, debug_info, windzone,
+    no_gfx, logging_errors, timing, shwo_arcs;
+int game_delay, time_slice, insanity, update_timer,
+    max_gx, max_gy, stats_mode;
+int game_limit, game_cycle, matches, played, executed;
+
+//general settings
+bool quit, report, show_cnotice;
+int kill_count, report_type;
+
+int main(void)
+{
+
+    return 0;
 }
+
