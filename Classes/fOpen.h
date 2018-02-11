865ad934830ffdfeabffc76ca92382967bddd1ba
wildog4 <wildog4@hotmail.com>
2018-02-11 11:41:21 -0500

    Created the header file for fOpen.cpp

---
+++ Classes/fOpen.h
@@ -0,0 +1,32 @@
+#ifndef FOPEN_H
+#define FOPEN_H
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <string.h>
+#include <cstdlib>
+
+using namespace std;
+
+class fOpen
+{
+    public:
+        fOpen();
+        fOpen(char inDirectory[]);
+        string getBuffer ();
+        void read_file_to_buffer(FILE *f);
+        void cleanup();
+        string getLine (int);
+        void writeLine (string, FILE *f);
+
+
+    private:
+        char inPath[80];
+        char outPath[80];
+        char * buffer;
+        FILE * roboFile;
+        bool isValidExt(char directory[]);
+        int findLine (int);
+};
+
+#endif
