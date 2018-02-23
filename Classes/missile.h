09d005ec1ccc53e9c536f90ca0999ec0237ec646
mcleanes <35745480+mcleanes@users.noreply.github.com>
2018-02-23 13:49:55 -0500

    Add files via upload

--- Classes/missile.h
+++ Classes/missile.h
@@ -1,55 +1,68 @@
 /* 
 Name: Stats.h	
 Desc: The purpose of this class is to store and
-		manage the stats of each robot. this means
-		calculating kills, deaths, wins, matches,
-		and shots.
+		manage the data of each missile, such as
+		the path it will travel, the damage it deals
+		and confirming a hit. 
 	Coder(s): Eric & Tyler
 	Date: 2/22/2018
-	Last Update: 2/22/2018
+	Last Update: 2/23/2018
 */
 
 #include <iostream>
 #include <stdio.h>
 #include <string>
 #include <iomanip>
-#include <cstdint>
+#include "Robot.h"
 using namespace std;
 
 
 const int MAX_MISSILES = 3; 
 const int TOTAL_ROBOTS = 5; 
-const int MAX_RANGE = 800; 
+const int MAX_RANGE = 80; 
 
 class Missile {
-	int speed[TOTAL_ROBOTS], damage[TOTAL_ROBOTS], xt[TOTAL_ROBOTS], 
-		yt[TOTAL_ROBOTS], xCor[MAX_RANGE], yCor[MAX_RANGE]; 
+	int speed, dam, lastShot; 
 		
-	int x, y; //our x and y position , assume 0,0 when 
-			//it is immediatly fired 		
+	double xCor[MAX_RANGE], yCor[MAX_RANGE]; 
 	
 	
 	public:
+	void missileInit(); 
 	void shoot(int); 
 	bool hitD(int); 
 	double damage(int); 
-	void line(int, int); 
+	void line(int, int); //x and y are doubles?
+	
+	//getters 
+	double getDam(); 
+	int getLastShot(); 
+	
 	
 }
-	//once the missle class is used, shouldn't we already know who shot last?
-void shoot(r) {
-	//r dictates robot
 
+void missileInit() {
+	dam = 0; 
+	speed = 1; 
+	
+	for (int cnt = 0; (cnt * speed) < MAX_RANGE; cnt++) { //wayy to large a range
+		yCor[cnt] = 0; 
+		xCor[cnt] = 0; 
+			
+	} 
+	
 }
 
-bool hitD(r) {	
-	//pass in x and y of target from somewhere else
-	//use x and y of missile from another func
+void shoot(r) {
+	lastShot = r; 
+}
+
+bool hitD(r, x, y) {	
 	bool hit =  false; 
 	int cnt = 0; 
 	
 	while (cnt < MAX_RANGE) {
-		if ((xt[r] == xCor[cnt]) && (yt[r] == yCor[cnt]))
+		if ((x == xCor[cnt]) && (y == yCor[cnt]))
 			hit = true;
 		
 		cnt++;
@@ -59,20 +72,9 @@ bool hitD(r) {
 }
 
 
-//damage assumes a hit has been confirmed, and we know
-//what robot is doing the damage
-double damage (r) {
-	double dam; 
+double damage (shotStrength) { 
 
-	
-	//the robot doing the damage
-	dam = robot[r].getdamageadj() * .80
-	
-	
-	//i don't know what the method is called,
-	//so correct this when available
-	
-	//dam gets deducted from the armor value of the robot
+	dam = shotStrength * .80; 
 	
 	return dam; 
 	
@@ -81,19 +83,18 @@ double damage (r) {
 //this is the line the missile will follow 	
 void line(x, y) {
 	//assume the robot is origin (0,0)
-	int m, b; 
+	double m, b; 
 	
 	//create slope
 	m = y/x; 
 	b = y / (m * x); 
 	
 	//creates the coordinates for the missile to follow, 
-	for (int cnt = 0; cnt < MAX_RANGE; cnt++) { //wayy to large a range
-		yCor[cnt] = (m * cnt) + b 
+	for (int cnt = 0; (cnt * speed) < MAX_RANGE; cnt++) { //wayy to large a range
+		yCor[cnt] = (m * cnt) + b; 
 		xCor[cnt] = cnt; 
 			
 	} 
-		
 	
 	//we can figure out what "quadrant" the
 	//target is in, and change direction depending on it 
@@ -101,3 +102,11 @@ void line(x, y) {
 	//do we even have to worry about quadrants?
 	
 }	
+
+double getDam() {
+	return dam; 
+}
+
+int getLastShot() {
+	return lastShot; 
+}
