813b5be1063beb083562bf1aac2b1f25e4f1bb4d
wildog4 <wildog4@hotmail.com>
2018-04-02 17:43:44 -0400

    updated version

--- Classes/fOpen.h
+++ Classes/fOpen.h
@@ -13,19 +13,25 @@ using namespace std;
 class fOpen
 {
     public:
+        //Constructor Opens up the windows prompt to take in a directory of a file
+        //and puts it in the inPath variable. If the directory isn't valid, then the
+        //program exits.
         fOpen();
+        //returns the variable buffer
         string getBuffer ();
-        void read_file_to_buffer(FILE *f);
-        void cleanup();
-        string getLine (int);
-        void writeLine (string, FILE *f);
+        //returns the variable inPath
+        char* getInPath ();
+
 
 
     private:
+
         char * inPath;
         char * buffer;
         FILE * roboFile;
         bool isValidExt(char directory[]);
+        void read_file_to_buffer(FILE *f);
+        void writeLine (string, FILE *f);
         int findLine (int);
         string ucase(string s);
 };
