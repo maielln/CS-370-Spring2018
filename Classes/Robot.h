c3e2bed0527cf56f5d94a61039f5dca2e090950c
miipha <35679376+miipha@users.noreply.github.com>
2018-04-03 14:14:56 -0400

    Update Robot.h

--- Classes/Robot.h
+++ Classes/Robot.h
@@ -1,42 +1,46 @@
-//Just the header for the robot class
-//shows all variables that are public
-//Contributors: Nicole Maiello
-
 #include <string>
 #include <cstdlib>
-#include <scan.h>
+#include "SimpleSwitch.h"
 
 #ifndef ROBOT
 #define ROBOT
+
+//Contributor(s): Nicole Maiello
+//shell of robot, holds all variables
 
 using namespace std;
 
 class Robot{
 
   public:
-    int max_ram; //ram is going to hold all registers
+    static const int max_ram = 1023, max_stack = 256; //ram is going to hold all registers
+    uint16_t RAM[max_ram]; //ram for 16 bit integers
+    string Stack[max_stack];
     int acceleration, turn_rate, crash_range, speed; //robot moving
     int shields, overburn, keepshift, cooling, scanrange;
     int x, y; //Robot coordinates
     int max_sonar, max_missiles, max_mines, max_queue, missile_spd, hit_range; //details
     int mines, shield, heatsinks;
-    int kills, wins, deaths, hits, health; //stats variables, can add/remove
-    double shotstrength, damageadj, speedadj;
+    int kills, wins, deaths, hits, shots; //stats variables, can add/remove
+    double shotstrength, damageadj, speedadj, health;
+    int turretAngle;
+    int arraySize;
+    int tarx = 0, tary = 0;
     bool shields_up;
     string Name;
-    Robot** robot_arr;
-    
+    int tarName;
+    string compiledFile;
+    int attackNum;
 
-    Robot(string, Robot**); //used to create robot
-    void init_Robot(); //initializes robot - ported directly from Confer's code
+
+
+    Robots(string, string); //initializes robot
     void robot_config(int, int, int, int, int, int, int);//note int order for this
-    void set_coordinates(int, int); //sets robot coordinates
+    void set_AttackArray();
+    string sendAttack();
 
   private:
     bool isLocked;
-
-   public:
-    uint16_t RAM[]; //ram for 16 bit integers
 };
 
 #endif // Robot
