60f2ea6cfb9662255cd256bfc09587f9c9c57ff2
miipha <35679376+miipha@users.noreply.github.com>
2018-04-03 18:58:21 -0400

    Update Format.h

--- Compile/Format.h
+++ Compile/Format.h
@@ -8,12 +8,22 @@ using namespace std;
 class Format
 {
 public:
-    int fireCount;
-    bool scanning;
-    int scanarc;
-
     string format(string);
-    string trim(string); //can be useful in other files
+    /*
+        This funciton takes in a string, then puts it through the rest of the code
+        so it can be sucessfully formatted.
+        EX.
+           Input:    OPO            8
+           Output:OPO,8
+    */
+    string trim(string);
+    /*
+        This funciton takes in a string, then removes whitespace from the front
+        and back of the text.
+        EX.
+            Input:        I want some food.         
+            Output:I want some food.
+    */
 
 private:
     void formatFile();
