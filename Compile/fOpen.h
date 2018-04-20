0e35498129cc38c81ee7ee4b1bb157e0bbdb6ad1
OriginalUsername1 <35743045+OriginalUsername1@users.noreply.github.com>
2018-04-20 14:14:00 -0400

    Update fOpen.h

--- Compile/fOpen.h
+++ Compile/fOpen.h
@@ -1,6 +1,7 @@
 #ifndef FOPEN_H
 #define FOPEN_H
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <stdio.h>
 #include <string.h>
@@ -10,18 +11,21 @@
 
 using namespace std;
 
-class fOpen
+/*
+File opening class
+Contributors: Mark Yetter, William Csont
+Written in C++ (gcc compiler) in the spring of 2018
+Purpose: to allow a program to open and read the contents of a file, as well as print into a different file.
+*/
+
+class fOpen //requires libcomdlg32.a
 {
     public:
-        //Constructor Opens up the windows prompt to take in a directory of a file
-        //and puts it in the inPath variable. If the directory isn't valid, then the
-        //program exits.
-        fOpen();
-        //returns the variable buffer
-        string getBuffer ();
-        //returns the variable inPath
-        string getInPath ();
 
+        fOpen(); //Constructor Opens up the windows prompt to take in a directory of a file and puts it in the inPath variable. If the directory isn't valid, then the program exits.
+        string getBuffer (); //returns the variable buffer
+        string getInPath (); //returns the variable inPath
+        void writeLine (string); //Writes a line to the output file. If no file exists, it creates one
 
 
     private:
@@ -30,9 +34,9 @@ class fOpen
         char * buffer;
         FILE * roboFile;
         string inPathStr;
+
         bool isValidExt(char directory[]);
         void read_file_to_buffer(FILE *f);
-        void writeLine (string, FILE *f);
         int findLine (int);
         string ucase(string s);
 };
