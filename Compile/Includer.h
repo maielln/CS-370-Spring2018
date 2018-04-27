6e46e5123500fe9af969a8ad2bd473be60dc8b37
wildog4 <wildog4@hotmail.com>
2018-04-27 12:28:54 -0400

    Create Includer.h

---
+++ Compile/Includer.h
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <iostream>
+#include <cmath>
+#include <string.h>
+#include <ctime>
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <windows.h>
+#include <sstream>
+
+
+using namespace std;
