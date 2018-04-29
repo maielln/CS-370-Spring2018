fa71e682e123442e57fbcb87ebfcf13a37deada6
OriginalUsername1 <35743045+OriginalUsername1@users.noreply.github.com>
2018-04-29 16:49:41 -0400

    Update comp.h

--- Compile/comp.h
+++ Compile/comp.h
@@ -1,6 +1,6 @@
 #ifndef COMP_H
 #define COMP_H
-#include "fOpen.h"
+#include <fOpen.h>
 //Contributer(s): Mark Yetter & Nicole Maiello
 //successfully formats code to be read correctly (ex. opo,17)
 using namespace std;
@@ -8,17 +8,21 @@ using namespace std;
 class Comp
 {
 public:
-    int fireCount;
-    bool scanning;
-    int scanarc;
+    int scanner, armor, engines, mines, heatsinks, weapon, shield;
 
-    string compile(string);
+    Comp(string); //Constructor replaced compile()
+    //string compile(string); //Formats the contents of the file and assigns configuration points. Replaced by constructor and getText()
+    string getText(void); //replaces the return of compile()
     string trim(string); //can be useful in other files
+    void dispConfig (void); //Displays the configuration values for debugging
 
 private:
-    void formatFile();
+    void formatFile(void);
     string formatLine(string);
     string replaceSpace(string);
+    void setConfig(void);
+    void setVars(int *, char);
+
     string fText;
 };
-#endif // COMP
+#endif
